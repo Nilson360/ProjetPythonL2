1916e84888bbcb7ccc2f44477f6e4fa3ef442530
Nilson360 <84663139+Nilson360@users.noreply.github.com>
2023-03-02 22:20:27 +0100

    update project

--- list01.py
+++ list01.py
@@ -5,10 +5,17 @@ Question 1
 Ecrire une fonction recevant en paramètre une liste de type L2, et renvoyant la liste composée des sommes des sous-listes d’origine.
 Par exemple, pour un paramètre égal à [[1,2],[3,2],[4,5,6]] la fonction doit renvoyer : [3,5,15]
 """
+
 list = [[1, 2], [3, 2], [4, 5, 6]]
 
-def sum_sublists(list):
-    return [sum(sublst) for sublst in list]
+def somme_sousLists(list):
+    return [sum(souslist) for souslist in list]
 
-sums = sum_sublists(list)
-print(sums)  # affiche [3, 5, 15]
+sums = somme_sousLists(list)
+print(sums)  # affiche -> [3, 5, 15]
+
+# Explications:
+"""
+La fonction somme_sousList utilise une compréhension de listes pour créer une nouvelle liste
+combiné avec la fonction sum() pour calculer la somme de  chaque sous liste : [sum(souslist) for souslist in list]
+"""
